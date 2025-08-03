490ab4f8828fbf5c73f0a829b1110cfb6ff1944a
Guilherme Luzin <guilherme.carvalho@inventsoftware.com.br>
2025-08-03 13:25:21 -0300

    retirando comentario

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,10 +14,6 @@ function Navbar({voltarVisivel}) {
             className="w-10 h-auto inline-block ml-2 hover:cursor-pointer"
             alt="Sra. Batata"
             onClick={() => navigate('/')} />
-
-        {/* <Link to="/" className="flex items-center gap-2 px-1 text-2xl font-bold ">
-          Sra. Batata
-        </Link> */}
         <nav className="space-x-4 flex">
           <h3 className='text-[#843E1B]'> | </h3>
           <Link to="/cardapio" className="gap-2 items-center">Cardápio</Link>

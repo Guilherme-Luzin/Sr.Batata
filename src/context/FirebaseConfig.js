21e9c5a55775589598647ed787904f8a30fbecb0
Guilherme-Luzin <guilherme.carvalho@inventsoftware.com.br>
2026-05-15 08:07:39 -0300

    Ajustando para usar variaveis de ambiente em vez de chaves fixas

--- src/context/FirebaseConfig.js
+++ src/context/FirebaseConfig.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYCTkWV7MLZvrSLcwphMHPT7KoG_uz_L0",
-  authDomain: "srabatata-76616.firebaseapp.com",
-  projectId: "srabatata-76616",
-  storageBucket: "srabatata-76616.firebasestorage.app",
-  messagingSenderId: "804816281019",
-  appId: "1:804816281019:web:782cbbaee63dd08146b89e",
-  measurementId: "G-WZKF3TY4CF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
